a0c9626126881cd717302915597ded542115be47
bojosteph <bojosteph@gmail.com>
2018-12-27 23:37:40 -0500

    adding genre class

---
+++ lib/TvShow/genre.rb
@@ -0,0 +1,24 @@
+
+class TvShow::Genre
+  include Initialize
+  include Findable
+ 
+  attr_accessor :show, :type, :name
+
+  @@all = []
+ 
+  def self.all
+    @@all
+  end
+    
+  def self.create_from_show_hash
+     TvShow::Show.all.collect do |show|
+       genre = TvShow::Genre.new
+       genre.show = show.name
+       genre.type = show.type
+       genre.name = show.genres
+      genre
+    end
+  end
+  
+end
